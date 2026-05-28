0cff70a0a64cf2b06e39efb9e48725043b516327
AL1US <193784287+AL1US@users.noreply.github.com>
2026-05-28 11:24:35 +0400

    add register

--- app/api/user/auth.py
+++ app/api/user/auth.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Form, Request
+import traceback
+
+from fastapi import APIRouter, Form, Request, status
+from fastapi.responses import JSONResponse
 
 from app.utils.frontend import templates
+from app.classes.user import LoginRequest
+from app.eth.blockchain_gateway import factory_client
 
 router = APIRouter()
 
@@ -14,8 +19,34 @@ def login(request: Request):
 
 # апи для пост
 @router.post("/login")
-def login():
-    pass
+def login(request: Request, data: LoginRequest):
+    pk = data.public_key
+    
+    try:
+        checksum_address = factory_client.w3.to_checksum_address(pk)
+
+        user_data = factory_client.contract.functions.users(checksum_address).call()
+         
+        user_name = pk[0] if isinstance(pk, tuple) else pk
+        
+        if not user_name:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={"error": "Ваш адрес не зарегистрирован в системе смарт-контракта."}
+            )
+        
+        return {"redirect": "/"}
+
+    except Exception as e:
+    # потом добавить логирование
+        print("КАКАЯ ТО ОШИБКА ПРИ РЕГИСТРАЦИИ (ВХОД В АККАУНТ)")
+        traceback.print_exc()
+        
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"error": f"Ошибка сервера: {str(e)}"}
+        )
+    
 # принять данные
 
 # вызвать метод регистрации если аккаунт не зареган 
